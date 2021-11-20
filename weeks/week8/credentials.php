1e054e9dbdb662dee08ff8afb317180c095cf036
WChihWen <65738834+WChihWen@users.noreply.github.com>
2021-11-19 20:39:22 -0800

    upload week8
    upload week8

---
+++ weeks/week8/credentials.php
@@ -0,0 +1,6 @@
+<?php
+    define('DB_NAME', 'it261db');
+    define('DB_USER', 'yaman0923');
+    define('DB_PASSWORD', 'Chihwen0923');
+    define('DB_HOST', 'it261db.mysql.database.azure.com');
+?>
