743029fe6f48a9b7025f6845f6e76684aa301caf
WChihWen <yaman0923@gmail.com>
2021-11-19 21:51:31 -0800

    uploadconfig

--- weeks/week8/config.php
+++ weeks/week8/config.php
@@ -10,7 +10,11 @@ include('credentials.php');
 
  
 
- $mydb="ignore";
+ echo 'DB_HOST:'.DB_HOST.'<br>';
+ echo 'DB_USER:'.DB_USER.'<br>';
+ echo 'DB_PASSWORD:'.DB_PASSWORD.'<br>'; 
+ echo 'DB_NAME:'.DB_NAME.'<br>';
+
 
  
 
---
+++ weeks/week8/credential.php
@@ -0,0 +1,15 @@
+<?php
+//define('DB_NAME', 'mydb');
+// define('DB_USER', 'yaman0923');
+// define('DB_PASSWORD', 'Chihwen0923');
+// define('DB_HOST', 'it261db.mysql.database.azure.com');
+
+define('DB_NAME', getenv("DB_NAME"));
+define('DB_USER', getenv("DB_USER"));
+define('DB_PASSWORD', getenv("DB_PASSWORD"));
+define('DB_HOST', getenv("DB_HOST"));
+
+
+
+
+?>
