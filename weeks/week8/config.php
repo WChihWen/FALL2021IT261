0ad843d641b82e2f23f11f41168c97a595ab53fb
WChihWen <yaman0923@gmail.com>
2021-11-19 21:56:31 -0800

    Test_getenvInfo

--- weeks/week8/config.php
+++ weeks/week8/config.php
@@ -10,11 +10,8 @@ include('credentials.php');
 
  
 
- echo 'DB_HOST:'.DB_HOST.'<br>';
- echo 'DB_USER:'.DB_USER.'<br>';
- echo 'DB_PASSWORD:'.DB_PASSWORD.'<br>'; 
- echo 'DB_NAME:'.DB_NAME.'<br>';
-
+ echo getenv("MYTEST");
+ 
 
  
 
