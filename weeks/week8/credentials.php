0810fac4af84e2e94988fb518816e89e448f62d3
WChihWen <65738834+WChihWen@users.noreply.github.com>
2021-11-19 20:46:26 -0800

    Delete credentials.php

--- weeks/week8/credentials.php
+++
@@ -1,6 +0,0 @@
-<?php
-    define('DB_NAME', 'it261db');
-    define('DB_USER', 'yaman0923');
-    define('DB_PASSWORD', 'Chihwen0923');
-    define('DB_HOST', 'it261db.mysql.database.azure.com');
-?>
