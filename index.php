3ca189c280e53b85e81ab261a3e4a000518d57a6
WChihWen <yaman0923@gmail.com>
2021-11-20 21:58:05 -0800

    upload aside.php

--- index.php
+++ index.php
@@ -33,65 +33,9 @@
       </div>        
    </div>
 </section>
-   <aside>
-      
+<aside>
+<?php include 'includes/aside.php';?>
+</aside>
 
-      <h2>Weekly Assignments</h2>
-      <h3>Week 1:</h3>
-      <ul>         
-         <li><a href="https://github.com/WChihWen/FALL2021IT261" target="_blank">GitHub</a></li>
-      </ul>
-      <h3>Week 2:</h3>
-      <ul>         
-         <li><a href="https://docs.google.com/document/d/18Pd7WKsm8putC0T0yO_cFMjQu7xcIIvALLY5HNNOyXU/edit" target="_blank">MAMP Notes</a></li>
-         <li><a href="weeks/week2/test_mamp.php" target="_blank">test_mamp.php</a></li>
-         <li><a href="weeks/week2/var.php" target="_blank">var.php</a></li>
-         <li><a href="weeks/week2/var2.php" target="_blank">var2.php</a></li>
-         <li><a href="weeks/week2/currency-logic.php" target="_blank">currency-logic.php</a></li>
-         <li><a href="weeks/week2/currency.php" target="_blank">currency.php</a></li>
-         <li><a href="weeks/week2/heredoc.php" target="_blank">heredoc.php</a></li>
-      </ul>      
-      <h3>Week 3:</h3>
-      <ul>
-         <li><a href="weeks/week3/if.php" target="_blank">if.php</a></li>
-         <li><a href="weeks/week3/date.php" target="_blank">date.php</a></li>
-         <li><a href="weeks/week3/foreach.php" target="_blank">foreach.php</a></li>
-         <li><a href="weeks/week3/forloop.php" target="_blank">forloop.php</a></li>         
-         <li><a href="weeks/week3/switch.php" target="_blank">switch.php</a></li>         
-      </ul>
-      <h3>Week 4:</h3>
-      <ul>
-         <li><a href="weeks/week4/form1.php" target="_blank">form1.php</a></li>
-         <li><a href="weeks/week4/form2.php" target="_blank">form2.php</a></li>
-         <li><a href="weeks/week4/form3.php" target="_blank">form3.php</a></li>
-         <li><a href="weeks/week4/form-arithmetic.php" target="_blank">form-arithmetic.php</a></li>
-         <li><a href="weeks/week4/celcius.php" target="_blank">celcius.php</a></li>    
-      </ul>
-      <h3>Week 5:</h3>
-      <ul>
-         <li><a href="weeks/week5/null.php" target="_blank">null.php</a></li>
-         <li><a href="weeks/week5/currency1.php" target="_blank">currency1.php</a></li>
-         <li><a href="weeks/week5/currency2.php" target="_blank">currency2.php</a></li>         
-         <li><a href="weeks/week5/currency3.php" target="_blank">currency3.php</a></li>     
-         <li><a href="weeks/week5/extra-credit.php" target="_blank">extra-credit.php</a></li>           
-      </ul>
-      <h3>Week 6:</h3>
-      <ul>
-         <li><a href="weeks/week6/form1.php" target="_blank">form1.php (Send an Email)</a></li> 
-         <li><a href="weeks/week6/functions.php" target="_blank">functions.php</a></li> 
-         <li><a href="weeks/week6/implode.php" target="_blank">implode.php</a></li> 
-      </ul>
-      <h3>Week 7:</h3>
-      <ul>
-         <li><a href="weeks/week7/random.php" target="_blank">random.php</a></li> 
-         <li><a href="weeks/week7/strings.php" target="_blank">strings.php</a></li> 
-         <li><a href="weeks/week7/pictures.php" target="_blank">pictures.php</a></li> 
-      </ul>
-      <h3>Week 8:</h3>
-      <ul>
-         <li><a href="weeks/week8/people.php" target="_blank">people.php</a></li> 
-      </ul>
-   </aside>
-   
 <?php include 'includes/footer.php';?>
     
