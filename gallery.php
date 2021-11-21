b146ab4cdfbd654129e2652a57b927ffde64818e
WChihWen <yaman0923@gmail.com>
2021-11-20 22:04:54 -0800

    update gallery.php

--- gallery.php
+++ gallery.php
@@ -1,11 +1,5 @@
 <?php include 'includes/header.php';?> 
 
-<script>
-  $( function() {
-    $( "#tabs" ).tabs();
-  } );
-</script>
-
 <section>  
 <?php   
     $iConn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die(myError(__FILE__,__LINE__,mysqli_connect_error()));
