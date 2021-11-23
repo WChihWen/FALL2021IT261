b7000000b4ca468704e26fdf0ab71a00b69a07b6
WChihWen <yaman0923@gmail.com>
2021-11-22 16:27:40 -0800

    update name

--- website/gallery.php
+++ website/gallery.php
@@ -2,9 +2,9 @@
 
 <?php
 $people['Robert_Downey_Jr'] = 'Robe_Anthony Edward "Tony" Stark was a billionaire industrialist, a founding member of the Avengers, and the former CEO of Stark Industries. A brash but brilliant inventor, Stark was self-described as a genius, billionaire, playboy, and philanthropist.';
-$people['Peter_Benjamin_Parker'] = 'Pete_Peter Benjamin Parker is a high school student who gained spider-like abilities, fighting crime across New York City as the alter ego Spider-Man, hoping someday, he will live up to his heroes in the Avengers.';
+$people['Tom_Holland'] = 'Pete_Peter Benjamin Parker is a high school student who gained spider-like abilities, fighting crime across New York City as the alter ego Spider-Man, hoping someday, he will live up to his heroes in the Avengers.';
 $people['Scarlett_Johansson'] = 'Scar_Natalia Alianovna "Natasha" Romanoff also known as Black Widow, was one of the most talented spies and assassins in the entire world and a founding member of the Avengers.';
-$people['Steven_Rogers'] = 'Stev_Captain Steven Grant "Steve" Rogers is a World War II veteran, a founding member of the Avengers, and Earth\'s first known superhero.';
+$people['Chris_Evans'] = 'Stev_Captain Steven Grant "Steve" Rogers is a World War II veteran, a founding member of the Avengers, and Earth\'s first known superhero.';
 $people['Chris_Hemsworth'] = 'Chri_hristopher "Chris" Hemsworth portrayed Thor in Thor, The Avengers, Thor: The Dark World, Avengers: Age of Ultron, Doctor Strange, Thor: Ragnarok, Avengers: Infinity War, Avengers: Endgame and Thor: Love and Thunder. He also portrayed an alternate Thor in Avengers: Endgame.';
 ?>
 
@@ -36,11 +36,8 @@ $people['Chris_Hemsworth'] = 'Chri_hristopher "Chris" Hemsworth portrayed Thor i
                         <td><img class="heroImg" src="images/<?php echo substr($image, 0, 4); ?>_1.jpg" alt="<?php echo str_replace('_',' ',$name); ?>"></td>
                     </tr>
                 <?php endforeach; ?>
-        </table>
-        
-
+            </table>    
          </article>
-
          <aside class="links" style="width:180px;">                
             <br>
          </aside>
