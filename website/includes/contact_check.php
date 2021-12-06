7e7447a291a7c198ef9b5e0a10bb6b877734529b
WChihWen <yaman0923@gmail.com>
2021-12-06 11:08:12 -0800

    go

--- website/includes/contact_check.php
+++ website/includes/contact_check.php
@@ -175,52 +175,54 @@
             $user = SENDGRID;
             $pass = SENDGRIDPWD;
 
+            print_r('User: '.$user.'<br> pass:'.$pass );
+
             // $json_string = array(
             //     'to' => array(
             //         'example1@sendgrid.com'
             //     ),
             //     'category' => 'test_category'
             // );
-            $json_string = array(
-                'to' => array(
-                    $to
-                )               
-            );
+            // $json_string = array(
+            //     'to' => array(
+            //         $to
+            //     )               
+            // );
 
 
-            $params = array(
-                'api_user'  => $user,
-                'api_key'   => $pass,
-                'x-smtpapi' => json_encode($json_string),
-                'to'        => $email,
-                'subject'   => $subject ,                
-                'text'      => $body,
-                'from'      => 'noreply@mystudentswa.com',
-            );
+            // $params = array(
+            //     'api_user'  => $user,
+            //     'api_key'   => $pass,
+            //     'x-smtpapi' => json_encode($json_string),
+            //     'to'        => $email,
+            //     'subject'   => $subject ,                
+            //     'text'      => $body,
+            //     'from'      => 'noreply@mystudentswa.com',
+            // );
 
 
-            $request =  $url.'api/mail.send.json';
+            // $request =  $url.'api/mail.send.json';
 
-            // Generate curl request
-            $session = curl_init($request);
-            // Tell curl to use HTTP POST
-            curl_setopt ($session, CURLOPT_POST, true);
-            // Tell curl that this is the body of the POST
-            curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-            // Tell curl not to return headers, but do return the response
-            curl_setopt($session, CURLOPT_HEADER, false);
-            // Tell PHP not to use SSLv3 (instead opting for TLS)
-            curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-            curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+            // // Generate curl request
+            // $session = curl_init($request);
+            // // Tell curl to use HTTP POST
+            // curl_setopt ($session, CURLOPT_POST, true);
+            // // Tell curl that this is the body of the POST
+            // curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+            // // Tell curl not to return headers, but do return the response
+            // curl_setopt($session, CURLOPT_HEADER, false);
+            // // Tell PHP not to use SSLv3 (instead opting for TLS)
+            // curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+            // curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
 
-            // obtain response
-            $response = curl_exec($session);
-            curl_close($session);
+            // // obtain response
+            // $response = curl_exec($session);
+            // curl_close($session);
 
-            // print everything out
-            print_r($response);
+            // // print everything out
+            // print_r($response);
             
-            exit;
+            // exit;
         }
     }
 
