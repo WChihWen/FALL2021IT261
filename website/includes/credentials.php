e8d6e32308ea43ba08821e065085f28c51d032ec
WChihWen <yaman0923@gmail.com>
2021-12-06 11:52:05 -0800

    go

--- website/includes/credentials.php
+++ website/includes/credentials.php
@@ -3,6 +3,6 @@ define('DB_NAME', getenv("DB_NAME"));
 define('DB_USER', getenv("DB_USER"));
 define('DB_PASSWORD', getenv("DB_PASSWORD"));
 define('DB_HOST', getenv("DB_HOST"));
-define('SENDGRID', getenv("SENDGRID"));
-define('SENDGRIDPWD', getenv("SENDGRIDPWD"));
+define('SGKEY', getenv("SGKEY"));
+define('SGUSERNAME', getenv("SGUSERNAME"));
 ?>
